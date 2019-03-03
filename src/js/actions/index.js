20f33f34da7967df6ef6ac61fa8a4a57caca4867
jericho <boker_boker@yahoo.com>
2019-03-03 21:44:52 +1100

    add post component attempt

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -3,3 +3,11 @@ import { ADD_ARTICLE } from '../constants/action-types';
 export function addArticle(payload) {
   return { type: ADD_ARTICLE, payload };
 }
+
+export function getData() {
+  return fetch('stats.nba.com/stats/scoreboard/?GameDate=02/14/2015&LeagueID=00&DayOffset=0')
+    .then(response => response.json())
+    .then(json => {
+      return { type: 'DATA_LOADED', payload: json };
+    });
+}
